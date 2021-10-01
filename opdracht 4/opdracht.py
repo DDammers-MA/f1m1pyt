bf32affc204ecd402f68a5e1c251401005c34a9f
Daniel <33257@ma-web.nl>
2021-10-01 10:28:49 +0200

    opdracht school tool

---
+++ opdracht 4/opdracht.py
@@ -0,0 +1,27 @@
+dag = "dinsdag"
+
+
+if dag == "maandag":
+  print("Pak je spullen en ga er heen!")
+else:
+  print("Lekker uitslapen!")
+
+if dag == "dinsdag":
+    print("weer naar school")
+else:
+    print("gewoon weer lekker naar bed")
+
+if dag == "woensdag":
+    print("nog maar 3 dagen school")
+else:
+    print("je heb een school dag gemist")
+ 
+if dag == "donderdag":
+    print ("ooh is het alweer donderdag")
+else:
+    print("big time rush")
+
+if dag == "vrijdag":
+    print("omg het is bijna weekend")
+else:
+    print("lekker de laste dag skippen")
